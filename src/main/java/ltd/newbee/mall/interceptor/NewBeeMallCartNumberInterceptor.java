1ee72d6c0f24932e9b1d33b98f04fcac1d679060
Andy <1793415887@qq.com>
2024-09-11 14:34:44 +0800

    Andy

--- src/main/java/ltd/newbee/mall/interceptor/NewBeeMallCartNumberInterceptor.java
+++ src/main/java/ltd/newbee/mall/interceptor/NewBeeMallCartNumberInterceptor.java
@@ -1,19 +1,13 @@
 package ltd.newbee.mall.interceptor;
 
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @Component
 public class NewBeeMallCartNumberInterceptor implements HandlerInterceptor {
-
-
-
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
