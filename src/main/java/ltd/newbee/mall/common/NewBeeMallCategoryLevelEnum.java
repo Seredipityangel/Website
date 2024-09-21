5bfce3ff58284f4bd8d809d7b334f455bcb06d25
Andy <1793415887@qq.com>
2024-09-21 20:57:15 +0800

    Andy

--- src/main/java/ltd/newbee/mall/common/NewBeeMallCategoryLevelEnum.java
+++ src/main/java/ltd/newbee/mall/common/NewBeeMallCategoryLevelEnum.java
@@ -1,3 +1,44 @@
 package ltd.newbee.mall.common;
 
 
+public enum NewBeeMallCategoryLevelEnum {
+
+    DEFAULT(0, "ERROR"),
+    LEVEL_ONE(1, "Classification at the first level"),
+    LEVEL_TWO(2, "Secondary classification"),
+    LEVEL_THREE(3, "Tertiary classification");
+
+    private int level;
+
+    private String name;
+
+    NewBeeMallCategoryLevelEnum(int level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+
+    public static NewBeeMallCategoryLevelEnum getNewBeeMallOrderStatusEnumByLevel(int level) {
+        for (NewBeeMallCategoryLevelEnum newBeeMallCategoryLevelEnum : NewBeeMallCategoryLevelEnum.values()) {
+            if (newBeeMallCategoryLevelEnum.getLevel() == level) {
+                return newBeeMallCategoryLevelEnum;
+            }
+        }
+        return DEFAULT;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/ltd/newbee/mall/common/NewBeeMallOrderStatusEnum.java
+++ src/main/java/ltd/newbee/mall/common/NewBeeMallOrderStatusEnum.java
@@ -1,4 +1,48 @@
 package ltd.newbee.mall.common;
 
 public enum NewBeeMallOrderStatusEnum {
+
+    DEFAULT(-9, "ERROR"),
+    ORDER_PRE_PAY(0, "To be paid"),
+    ORDER_PAID(1, "paid"),
+    ORDER_PACKAGED(2, "Distribution completed"),
+    ORDER_EXPRESS(3, "Successful exit"),
+    ORDER_SUCCESS(4, "Successful transaction"),
+    ORDER_CLOSED_BY_MALLUSER(-1, "manual shutdown"),
+    ORDER_CLOSED_BY_EXPIRED(-2, "timeout"),
+    ORDER_CLOSED_BY_JUDGE(-3, "Merchant closure");
+
+    private int orderStatus;
+
+    private String name;
+
+    NewBeeMallOrderStatusEnum(int orderStatus, String name) {
+        this.orderStatus = orderStatus;
+        this.name = name;
+    }
+
+    public static NewBeeMallOrderStatusEnum getNewBeeMallOrderStatusEnumByStatus(int orderStatus) {
+        for (NewBeeMallOrderStatusEnum newBeeMallOrderStatusEnum : NewBeeMallOrderStatusEnum.values()) {
+            if (newBeeMallOrderStatusEnum.getOrderStatus() == orderStatus) {
+                return newBeeMallOrderStatusEnum;
+            }
+        }
+        return DEFAULT;
+    }
+
+    public int getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(int orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
