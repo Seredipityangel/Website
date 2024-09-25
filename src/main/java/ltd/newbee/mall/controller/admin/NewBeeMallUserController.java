4e50def3d33a7dffa192b5ea6373c1282f96cf55
Andy <1793415887@qq.com>
2024-09-25 13:42:52 +0800

    Andy

--- src/main/java/ltd/newbee/mall/controller/admin/NewBeeMallUserController.java
+++ src/main/java/ltd/newbee/mall/controller/admin/NewBeeMallUserController.java
@@ -1,4 +1,60 @@
 package ltd.newbee.mall.controller.admin;
 
+
+import ltd.newbee.mall.service.NewBeeMallUserService;
+import ltd.newbee.mall.util.PageQueryUtil;
+import ltd.newbee.mall.util.Result;
+import ltd.newbee.mall.util.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/admin")
 public class NewBeeMallUserController {
+
+    @Resource
+    private NewBeeMallUserService newBeeMallUserService;
+
+    @GetMapping("/users")
+    public String usersPage(HttpServletRequest request) {
+        request.setAttribute("path", "users");
+        return "admin/newbee_mall_user";
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping(value = "/users/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (ObjectUtils.isEmpty(params.get("page")) || ObjectUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("parameter anomaly！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(newBeeMallUserService.getNewBeeMallUsersPage(pageUtil));
+    }
+
+    /**
+     * 用户禁用与解除禁用(0-未锁定 1-已锁定)
+     */
+    @RequestMapping(value = "/users/lock/{lockStatus}", method = RequestMethod.POST)
+    @ResponseBody
+    public Result delete(@RequestBody Integer[] ids, @PathVariable int lockStatus) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("parameter anomaly！");
+        }
+        if (lockStatus != 0 && lockStatus != 1) {
+            return ResultGenerator.genFailResult("failure to comply！");
+        }
+        if (newBeeMallUserService.lockUsers(ids, lockStatus)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("Failed to disable");
+        }
+    }
 }
